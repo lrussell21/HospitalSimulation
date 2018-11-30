3ff7dd27373d6adb57cc8570f0a920da4312eef3
Pragalva Dhungana <pdhungana21@my.whitworth.edu>
2018-11-30 09:56:39 -0800

    Pateints done

--- Simulation/Simulation/Patients.h
+++ Simulation/Simulation/Patients.h
@@ -1,1 +1,24 @@
-#pragma once
+#ifndef _PATIENTS_H_
+#define _PATIENTS_H_
+
+class Patients
+{
+	public:
+		Patients(int t)
+		{
+			 patient_id = -1;
+			 check_in = t;
+			 wait_time = -1;
+			 treat_time = -1;
+			 priority_number = -1;
+			 patients_count++;
+		};
+		static int patients_count = 0;
+		int patient_id;
+		int check_in;
+		int wait_time;
+		int treat_time;
+		int priority_number;
+
+};
+#endif
